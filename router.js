33e256e5f03b183d72fc30ed389f81c6cb84a58e
Kron1749 <drakgr98@gmail.com>
2022-08-12 12:49:32 +0300

    Added favorite page

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 import Stories from "./pages/stories.js"
 import Item from "./pages/item.js"
+import Favorites from "./pages/favorites.js"
 
 const router = new Navigo(null, true, "#")
 
@@ -15,6 +16,7 @@ export default class RouterHandler {
             { path: "/ask", page: Stories },
             { path: "/show", page: Stories },
             { path: "/item", page: Item },
+            { path: "/favorites", page: Favorites },
         ]
         routes.forEach(({ path, page }) => {
             router
