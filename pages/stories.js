af8d143a947ba8149f2cd6bce0a490297b1f391a
Kron1749 <drakgr98@gmail.com>
2022-08-12 10:27:33 +0300

    Getting stories from API

--- pages/stories.js
+++ pages/stories.js
@@ -1,5 +1,22 @@
 import view from "../utils/view.js"
 
-export default function Stories(path) {
-    view.innerHTML = `<div>${path}</div>`
+export default async function Stories(path) {
+    const stories = await getStories(path)
+    const hasStories = stories.length > 0
+    view.innerHTML = `<div>
+        ${hasStories ? stories.map((storie) => JSON.stringify(storie)) : "No stories"}
+    </div>`
+}
+
+async function getStories(path) {
+    const isHomeRoute = path === "/"
+    const isNewRoute = path === "/new"
+    if (isHomeRoute) {
+        path = "/news"
+    } else if (isNewRoute) {
+        path = "/newest"
+    }
+    const response = await fetch(`https://node-hnapi.herokuapp.com${path}`)
+    const stories = await response.json()
+    return stories
 }
